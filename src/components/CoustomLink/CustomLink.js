4d75abd1230c29d0b94a7640a39379727879f359
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-03-31 20:43:56 +0600

    coustom link

---
+++ src/components/CoustomLink/CustomLink.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link, useMatch, useResolvedPath } from 'react-router-dom';
+
+function CustomLink({ children, to, ...props }) {
+    let resolved = useResolvedPath(to);
+    let match = useMatch({ path: resolved.pathname, end: true });
+
+    return (
+        <div>
+            <Link
+                style={{ color: match ? 'red' : 'black', fontWeight: match ? "bold" : "normal" }}
+                to={to}
+                {...props}
+            >
+                {children}
+            </Link>
+
+        </div>
+    );
+}
+
+
+
+export default CustomLink;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import CustomLink from '../CoustomLink/CustomLink';
+import './Header.css'
 
 const Header = () => {
     return (
         <div>
-            <h1>this header</h1>
+
+            <h1>React Router Header</h1>
+            <Nav variant="tabs" defaultActiveKey="/home">
+                <Nav.Item>
+                    <CustomLink className=' Link  btn-outline-dark' to="/home">Home</CustomLink>
+                </Nav.Item>
+                <Nav.Item>
+                    <CustomLink className=' Link btn-outline-dark' to="/friends">Friends</CustomLink>
+                </Nav.Item>
+                <Nav.Item>
+                    <CustomLink className='Link btn-outline-dark' to="/about" >
+                        About
+                    </CustomLink>
+                </Nav.Item>
+            </Nav>
         </div>
     );
 };
